7fff7eb9f8d8188e3769e03b112b7d4225140028
Matheus Flores <Maths.Aki@gmail.com>
2025-04-24 19:59:54 -0300

    beforechat

---
+++ back/src/APIs/top.rattet.ts
@@ -0,0 +1,20 @@
+let page: number = 1;
+let lang: string = 'en-US';
+const setLang = [
+  'en-US',
+  
+]
+
+const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`;
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: process.env.API_KEY
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
