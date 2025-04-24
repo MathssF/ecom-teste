515dcb5b3f8d3000d2b3c6e4884f423089567c68
Matheus Flores <Maths.Aki@gmail.com>
2025-04-24 20:08:39 -0300

    trend

--- back/src/APIs/top.rattet.ts
+++ back/src/APIs/top.rattet.ts
@@ -26,3 +26,6 @@ fetch(url, options)
   .then(res => res.json())
   .then(json => console.log(json))
   .catch(err => console.error(err));
+//
+
+export {};
---
+++ back/src/APIs/trends.ts
@@ -0,0 +1,16 @@
+const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: process.env.API_KEY
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
+//
+
+export {};
