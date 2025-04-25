c960c2909ceb31d4b5cba47947b711e0df6b9986
Matheus Flores <Maths.Aki@gmail.com>
2025-04-25 13:41:43 -0300

    env

--- global/fetchTMDB/top.rated.ts
+++ global/fetchTMDB/top.rated.ts
@@ -1,50 +1,54 @@
-import { setLang } from '../variables';
+export class TopRatedMovies {
+  private language: string;
+  private apiKey: string;
 
-let page: number = 1;
-let lang: string = 'en-US';
-
-const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`;
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: process.env.API_KEY!
-  }
-};
-
-async function getTopRatedMovies() {
-  let allMovies: any[] = [];
-
-  // Iterando pelas páginas de 1 a 13
-  for (let page = 1; page <= 13; page++) {
-    const response = await fetch(`${url}&page=${page}`, options);
-    const json = await response.json();
-
-    // Filtrando os dados que você quer
-    const movies = json.results.map((movie: any) => ({
-      id: movie.id,
-      title: movie.title,
-      original_title: movie.original_title,
-      original_language: movie.original_language,
-      vote_count: movie.vote_count,
-      vote_average: movie.vote_average,
-      popularity: movie.popularity,
-      adult: movie.adult,
-      release_date: movie.release_date,
-      genre_ids: movie.genre_ids,
-      poster_path: movie.poster_path,
-    }));
-
-    allMovies = [...allMovies, ...movies];
-
-    // Se já tivermos 250 elementos, podemos parar
-    if (allMovies.length >= 250) break;
+  constructor(language: string = 'en-US', apiKey: string) {
+    this.language = language;
+    this.apiKey = apiKey;
   }
 
-  // Garantir que vamos retornar exatamente 250 itens
-  return allMovies.slice(0, 250);
+  private getUrl(): string {
+    return `https://api.themoviedb.org/3/movie/top_rated?language=${this.language}`;
+  }
+
+  private getOptions(): RequestInit {
+    return {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: this.apiKey,
+      },
+    };
+  }
+
+  public async getTopRatedMovies(): Promise<any[]> {
+    let allMovies: any[] = [];
+
+    for (let page = 1; page <= 13; page++) {
+      const response = await fetch(`${this.getUrl()}&page=${page}`, this.getOptions());
+      const json = await response.json();
+
+      const movies = json.results.map((movie: any) => ({
+        id: movie.id,
+        title: movie.title,
+        original_title: movie.original_title,
+        original_language: movie.original_language,
+        vote_count: movie.vote_count,
+        vote_average: movie.vote_average,
+        popularity: movie.popularity,
+        adult: movie.adult,
+        release_date: movie.release_date,
+        genre_ids: movie.genre_ids,
+        poster_path: movie.poster_path,
+      }));
+
+      allMovies = [...allMovies, ...movies];
+
+      if (allMovies.length >= 250) break;
+    }
+
+    return allMovies.slice(0, 250);
+  }
 }
 
-getTopRatedMovies()
-  .then(movies => console.log(movies))
-  .catch(err => console.error(err));
+export default TopRatedMovies;
--- global/fetchTMDB/trendings.ts
+++ global/fetchTMDB/trendings.ts
@@ -1,49 +1,56 @@
-import { setLang, set_time_window } from '../variables';
+export class TrendingMovies {
+  private time_window: string;
+  private language: string;
+  private apiKey: string;
 
-let time_window: string = 'day';
-
-const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: process.env.API_KEY!
-  }
-};
-
-async function getTrendingMovies() {
-  let allMovies:any[] = [];
-
-  // Iterando pelas páginas de 1 a 13
-  for (let page = 1; page <= 13; page++) {
-    const response = await fetch(`${url}&page=${page}`, options);
-    const json = await response.json();
-
-    // Filtrando os dados que você quer
-    const movies = json.results.map((movie: any) => ({
-      id: movie.id,
-      title: movie.title,
-      original_title: movie.original_title,
-      original_language: movie.original_language,
-      vote_count: movie.vote_count,
-      vote_average: movie.vote_average,
-      popularity: movie.popularity,
-      adult: movie.adult,
-      release_date: movie.release_date,
-      genre_ids: movie.genre_ids,
-      poster_path: movie.poster_path,
-    }));
-
-    allMovies = [...allMovies, ...movies];
-
-    // Se já tivermos 250 elementos, podemos parar
-    if (allMovies.length >= 250) break;
+  constructor(time_window: string = 'day', language: string = 'en-US', apiKey: string) {
+    this.time_window = time_window;
+    this.language = language;
+    this.apiKey = apiKey;
   }
 
-  // Garantir que vamos retornar exatamente 250 itens
-  return allMovies.slice(0, 250);
+  private getUrl(): string {
+    return `https://api.themoviedb.org/3/trending/movie/${this.time_window}?language=${this.language}`;
+  }
+
+  private getOptions(): RequestInit {
+    return {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: this.apiKey,
+      },
+    };
+  }
+
+  public async getTrendingMovies(): Promise<any[]> {
+    let allMovies: any[] = [];
+
+    for (let page = 1; page <= 13; page++) {
+      const response = await fetch(`${this.getUrl()}&page=${page}`, this.getOptions());
+      const json = await response.json();
+
+      const movies = json.results.map((movie: any) => ({
+        id: movie.id,
+        title: movie.title,
+        original_title: movie.original_title,
+        original_language: movie.original_language,
+        vote_count: movie.vote_count,
+        vote_average: movie.vote_average,
+        popularity: movie.popularity,
+        adult: movie.adult,
+        release_date: movie.release_date,
+        genre_ids: movie.genre_ids,
+        poster_path: movie.poster_path,
+      }));
+
+      allMovies = [...allMovies, ...movies];
+
+      if (allMovies.length >= 250) break;
+    }
+
+    return allMovies.slice(0, 250);
+  }
 }
 
-getTrendingMovies()
-  .then(movies => console.log(movies))
-  .catch(err => console.error(err));
+export default TrendingMovies;
