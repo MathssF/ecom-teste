84d1f617e8254cabe24532b4971ece1f62fac113
Matheus Flores <Maths.Aki@gmail.com>
2025-04-25 20:13:14 -0300

    clear

--- back/src/basic/dto/update-basic.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateBasicDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_basic_dto_1 = require("./create-basic.dto");
-class UpdateBasicDto extends (0, mapped_types_1.PartialType)(create_basic_dto_1.CreateBasicDto) {
-}
-exports.UpdateBasicDto = UpdateBasicDto;
-//# sourceMappingURL=update-basic.dto.js.map
--- back/src/trendings/entities/trending.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Trending = void 0;
-class Trending {
-}
-exports.Trending = Trending;
-//# sourceMappingURL=trending.entity.js.map
