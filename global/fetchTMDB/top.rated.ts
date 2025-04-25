721f552c3ca6f5244e3ecc67e59a79b84a4d1097
Matheus Flores <Maths.Aki@gmail.com>
2025-04-25 05:26:21 -0300

    api

--- global/fetchTMDB/top.rated.ts
+++ global/fetchTMDB/top.rated.ts
@@ -12,10 +12,39 @@ const options = {
   }
 };
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
-//
+async function getTopRatedMovies() {
+  let allMovies: any[] = [];
 
-export {};
+  // Iterando pelas páginas de 1 a 13
+  for (let page = 1; page <= 13; page++) {
+    const response = await fetch(`${url}&page=${page}`, options);
+    const json = await response.json();
+
+    // Filtrando os dados que você quer
+    const movies = json.results.map((movie: any) => ({
+      id: movie.id,
+      title: movie.title,
+      original_title: movie.original_title,
+      original_language: movie.original_language,
+      vote_count: movie.vote_count,
+      vote_average: movie.vote_average,
+      popularity: movie.popularity,
+      adult: movie.adult,
+      release_date: movie.release_date,
+      genre_ids: movie.genre_ids,
+      poster_path: movie.poster_path,
+    }));
+
+    allMovies = [...allMovies, ...movies];
+
+    // Se já tivermos 250 elementos, podemos parar
+    if (allMovies.length >= 250) break;
+  }
+
+  // Garantir que vamos retornar exatamente 250 itens
+  return allMovies.slice(0, 250);
+}
+
+getTopRatedMovies()
+  .then(movies => console.log(movies))
+  .catch(err => console.error(err));
