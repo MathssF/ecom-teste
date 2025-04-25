f2a8a845d96c4003a2cc9cd2e333c1ca84377a40
Matheus Flores <Maths.Aki@gmail.com>
2025-04-25 18:19:29 -0300

    env

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.
+const port = process.env.PORT_BACK || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(port);
 }
 
 bootstrap();
