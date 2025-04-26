096c32ed36195fd83958234b3b3567aad2b085d8
Matheus Flores <Maths.Aki@gmail.com>
2025-04-26 01:39:31 -0300

    Prisma Dev Tables

--- global/workers/toBack.worker.ts
+++ global/workers/toBack.worker.ts
@@ -1,10 +1,11 @@
 export class ToBackWorker {
-  private data: any;
-  private path: string;
-  private count: number;
-  constructor(d: any, p: string, c: number) {
-    this.data = d;
-    this.path = p;
-    this.count = c;
+  static async sendOnlyFetch(path: string, method: string) {
+    try {
+      await fetch(path, {
+        method
+      })
+    } catch(err) {
+      
+    }
   }
 }
