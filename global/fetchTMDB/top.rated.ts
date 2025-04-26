f70a90051610a83ba03ae5412a2f9a9addd42a40
Matheus Flores <Maths.Aki@gmail.com>
2025-04-26 01:07:44 -0300

    more

--- global/fetchTMDB/top.rated.ts
+++ global/fetchTMDB/top.rated.ts
@@ -1,25 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-interface Movie {
-  id: number;
-  title: string;
-  original_title: string;
-  original_language: string;
-  vote_count: number;
-  vote_average: number;
-  popularity: number;
-  adult: boolean;
-  release_date: string;
-  genre_ids: number[];
-  poster_path: string;
-}
-
-interface TopRatedMoviesResponse {
-  results: Movie[];
-  total_pages: number;
-}
-
 export class TopRatedMoviesAPI {
   private language: string;
   private apiKey: string;
@@ -44,29 +25,20 @@ export class TopRatedMoviesAPI {
   }
 
   public async getTopRatedMovies(): Promise<{
-    allMovies: any[], pageSelects: { page: number, results: number }[]
+    allMovies: any[],
+    // pageSelects: { page: number, results: number }[]
   }> {
     let allMovies: any[] = [];
-    let pageSelects: { page: number, results: number }[] = [];
+    // let pageSelects: { page: number, results: number }[] = [];
+
     let page = 1;
     let totalPages = 1;
-  
+
     while (allMovies.length < 250 && page <= totalPages) {
-      console.log(`Fazendo requisição para a página ${page}`);
-      
       const response = await fetch(`${this.getUrl()}&page=${page}`, this.getOptions());
-      console.log(`Resposta recebida para a página ${page}:`, response.status);
-  
-      const json = await response.json();
-      console.log(`Dados da página ${page}:`, json);
-  
-      const { results, total_pages } = json as TopRatedMoviesResponse;
-      totalPages = total_pages;
+      const json: any = await response.json();
 
-      // console.log('Final das requisições');
-
-  
-      const movies = results.map((movie: any) => ({
+      const movies = json.results.map((movie: any) => ({
         id: movie.id,
         title: movie.title,
         original_title: movie.original_title,
@@ -79,22 +51,17 @@ export class TopRatedMoviesAPI {
         genre_ids: movie.genre_ids,
         poster_path: movie.poster_path,
       }));
-  
-      pageSelects.push({ page, results: results.length });
-  
+
       allMovies = [...allMovies, ...movies];
-  
+      // pageSelects.push({ page, results: movies.length });
+
+      totalPages = json.total_pages;
       page++;
     }
 
-    console.log('Final das Páginas, Última: ', page);
-    console.log('Page Selects: ', pageSelects);
-    console.log('Movie 1: ', allMovies[1]);
-    console.log('Moovie 200: ', allMovies[200]);
-  
     return {
       allMovies: allMovies.slice(0, 250),
-      pageSelects
+      // pageSelects
     };
   }
 }
