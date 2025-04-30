e48c36810e32e6ffe3e6a3ce8c60d8c9ff0365ec
Matheus Flores <Maths.Aki@gmail.com>
2025-04-30 01:46:15 -0300

    topRateds

--- front/src/contexts/top-rated.context.ts
+++ front/src/contexts/top-rated.context.ts
@@ -1,11 +1,9 @@
 'use client';
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { TopRatedMoviesAPI } from '../../../global/fetchTMDB/top-rated';
+import { TopRatedMoviesAPI } from '../../../global/fetchTMDB/top.rated';
 import { limitsData } from '../../../global/tables/interfaces';
 import {
   MovieData,
-  TopRatedList,
-  TopRatedResult,
   TopRatedContextType,
   TopRatedPageContextType,
 } from './interfaces/top-rated.interfaces';
@@ -14,7 +12,6 @@ const TopRatedContext = createContext<TopRatedContextType | undefined>(undefined
 const TopRatedPageContext = createContext<TopRatedPageContextType | undefined>(undefined);
 
 export const TopRatedProvider = () => {
-  // const [movieItem, setMovieItem] useState<MovieData | null>(null);
   const [movies, setMovies] = useState<MovieData[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -36,6 +33,52 @@ export const TopRatedProvider = () => {
   };
 
   return (
-    <TopRatedContext></TopRatedContext>
+    <TopRatedContext.Provider value={{ movies, loading, error, fetchMovie }}>
+      {children}
+    </TopRatedContext>
   )
 }
+
+export const useRated = () => {
+  const context = useContext(TopRatedContext);
+  if (context === undefined) {
+    throw new Error('');
+  }
+  return context;
+};
+
+export const TopRatedPagesProvider = () => {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTopRatedPage = async (page: number, data?: limitsData) =>  {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const api = new TopRatedMoviesAPI();
+      const topList = await api.getTopsByPage(page, data);
+      setMovies(topList)
+    } catch (err: any) {
+      setError(err.message || 'Erro ao buscar os Top Rateds');
+      setMovies(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <TopRatedPageContext.Provider value={{ movies, loading, error, fetchMovie }}>
+      {children}
+    </TopRatedPageContext>
+  )
+}
+
+export const useRatedPages = () => {
+  const context = useContext(TopRatedContext);
+  if (context === undefined) {
+    throw new Error('');
+  }
+  return context;
+};
