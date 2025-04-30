df546dee2cce509a303262bb3cea77feffc8deb8
Matheus Flores <Maths.Aki@gmail.com>
2025-04-30 00:03:59 -0300

    next

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+      </Head>
+      <main>
+        <h1>Hello, world!</h1>
+      </main>
+    </>
+  );
+}
