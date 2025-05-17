ba5b5f48a5a7847fce8acaa407979a4628342d63
Matheus Flores <Maths.Aki@gmail.com>
2025-05-17 00:45:02 -0300

    more

--- global/fetchTMDB/top.rated.ts
+++ global/fetchTMDB/top.rated.ts
@@ -217,6 +217,116 @@ export class TopRatedMoviesAPI {
     return allMovies;
   }
 
+  // public async getTopRatedMovies(data?: limitsData): Promise<any[] | any> {
+  //   let allMovies: TopRatedResult[] = [];
+  //   let allPages: number[] = [];
+  //   let resultsPages: TopRatedList[] = [];
+  //   let coupledData = [0, 0]; // [total_pages, total_results]
+  
+  //   let page = 1;
+  //   let totalPages = 1;
+  //   let maxItems = 250;
+  //   let maxPages = 13;
+  
+  //   // Limites e configurações
+  //   if (data?.setLimitItems && data.limitItems && data.limitItems > 0 && data.limitItems <= 1000) {
+  //     maxItems = data.limitItems;
+  //   }
+  //   if (data?.setLimitPages && data.limitPages && data.limitPages > 0 && data.limitPages <= 50) {
+  //     maxPages = data.limitPages;
+  //   }
+  //   if (data?.setLang && data.chooseLang) {
+  //     this.language = data.chooseLang;
+  //   }
+  
+  //   while (page <= totalPages && page <= maxPages && allMovies.length < maxItems) {
+  //     const response = await fetch(`${this.getUrl()}&page=${page}`, this.getOptions());
+  //     const json: any = await response.json();
+  
+  //     // Atualiza total pages e results se necessário
+  //     if (json.total_pages && json.total_pages > coupledData[0]) {
+  //       coupledData[0] = json.total_pages;
+  //     }
+  //     if (json.total_results && json.total_results > coupledData[1]) {
+  //       coupledData[1] = json.total_results;
+  //     }
+  
+  //     // Mapeia os filmes para TopRatedResult completo
+  //     const mappedMovies: TopRatedResult[] = json.results.map((movie: any, index: number) => {
+  //       const baseMovie: TopRatedResult = {
+  //         adult: movie.adult,
+  //         backdrop_path: movie.backdrop_path,
+  //         genre_ids: movie.genre_ids,
+  //         id: movie.id,
+  //         original_language: movie.original_language,
+  //         original_title: movie.original_title,
+  //         overview: movie.overview,
+  //         popularity: movie.popularity,
+  //         poster_path: movie.poster_path,
+  //         release_date: movie.release_date,
+  //         title: movie.title,
+  //         video: movie.video,
+  //         vote_average: movie.vote_average,
+  //         vote_count: movie.vote_count,
+  //       };
+  
+  //       // Adiciona campos opcionais conforme flags
+  //       if (data?.frontEndPage || data?.dev) {
+  //         baseMovie.page = page;
+  //       }
+  //       if (data?.dev) {
+  //         const globalIndex = allMovies.length + index + 1;
+  //         baseMovie.rank = globalIndex;
+  //         baseMovie.rankPage = index + 1;
+  //       }
+  
+  //       return baseMovie;
+  //     });
+  
+  //     // Prioridade 1: retornar TopRatedListWithPages
+  //     if (data?.returnPageList) {
+  //       allPages.push(page);
+  //       allMovies.push(...mappedMovies);
+  //     }
+  
+  //     // Prioridade 2: retornar TopRatedList[]
+  //     if (data?.resultModeByPage) {
+  //       const pageResult: TopRatedList = {
+  //         page,
+  //         results: mappedMovies,
+  //         total_pages: json.total_pages,
+  //         total_results: json.total_results,
+  //       };
+  //       resultsPages.push(pageResult);
+  //     }
+  
+  //     // Se for só array direto
+  //     if (!data?.returnPageList && !data?.resultModeByPage) {
+  //       allMovies.push(...mappedMovies);
+  //     }
+  
+  //     totalPages = json.total_pages;
+  //     if (allMovies.length >= maxItems) break;
+  //     page++;
+  //   }
+  
+  //   if (data?.returnPageList) {
+  //     const resultData: TopRatedListWithPages = {
+  //       pages: allPages,
+  //       movies: allMovies,
+  //       total_pages: coupledData[0],
+  //       total_results: coupledData[1],
+  //     };
+  //     return resultData;
+  //   }
+  
+  //   if (data?.resultModeByPage) {
+  //     return resultsPages; // TopRatedList[]
+  //   }
+  
+  //   return allMovies; // TopRatedResult[]
+  // }
+
   public async getTopsByPage(page: number, data?: limitsData): Promise<any> {
     if (data?.setLang && data.chooseLang) {
       this.language = data.chooseLang;
