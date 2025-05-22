7fb8ef53c04abc55d9c065305d5de7682b373d88
Matheus Flores <Maths.Aki@gmail.com>
2025-05-22 03:44:51 -0300

    more

--- back/src/basic/basic.service.ts
+++ back/src/basic/basic.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BasicRepository } from '../application/basic/basic.repository';
 import { GenreEntity } from './entities/basic.entity';
 import { CompaniesRepository } from '../application/basic/companies.repository';
-import { BasicSeed } from './seeds/basic.seeds';
+// import { BasicSeed } from './seeds/basic.seeds';
 
 
 @Injectable()
@@ -10,12 +10,12 @@ export class BasicService {
   constructor(
     private readonly basic: BasicRepository,
     private readonly companies: CompaniesRepository,
-    private readonly basicSeed: BasicSeed,
+    // private readonly basicSeed: BasicSeed,
   ) {}
 
-  async seedStart() {
-    return await this.basicSeed.run()
-  }
+  // async seedStart() {
+  //   return await this.basicSeed.run()
+  // }
 
 
   async findGenreId(id: string): Promise<GenreEntity | null> {
