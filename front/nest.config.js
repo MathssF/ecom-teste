a8c071a0d8f35a9877e71ef75d5e555f0e5ded83
Matheus Flores <Maths.Aki@gmail.com>
2025-05-22 03:07:33 -0300

    manyThings

--- front/nest.config.js
+++ front/nest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    webpack(config) {
+      config.resolve.alias['@global'] = path.resolve(__dirname, '../global');
+      return config;
+    },
   };
   
   module.exports = nextConfig;
--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import Header from '../components/header.components';
 
-// import './globals.css'; // Se você tiver um global CSS opcional
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
